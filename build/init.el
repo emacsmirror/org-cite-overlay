db3a725a4bddc9d67163e4e2ede5f4b0c35ba775
Samuel W. Flint <swflint@flintfam.org>
2024-02-06 22:08:55 -0600

    Add a build recipe

---
+++ build/init.el
@@ -0,0 +1,31 @@
+;;; init.el ---                                      -*- lexical-binding: t; -*-
+
+;; SPDX-FileCopyrightText: 2024 Samuel W. Flint <me@samuelwflint.com>
+;;
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(defvar build/required-packages
+  '(;; Linting, Style Checking, Packaging
+    package-lint
+    package-build
+
+    ;; Dependencies
+    citeproc
+    universal-sidecar
+    universal-sidecar-citeproc)
+  "Packages required for building the various recipes.")
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(defun build/package-install-prereqs ()
+  (package-refresh-contents)
+  (dolist (pkg build/required-packages)
+    (package-install pkg)))
+
+(defun build/build-packages ()
+  (require 'package-build)
+  (setf package-build-archive-dir (expand-file-name "~/package-archive/")
+        package-build-recipes-dir (expand-file-name "~/org-cite-overlay/build/recipes/"))
+  (package-build-all))
